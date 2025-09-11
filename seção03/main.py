c19f0d8ffdacc1a1f48619314c61712f22342b09
EnzoBustos <enzobustos@usp.br>
2025-09-10 22:23:35 -0300

    Seção 03 - Curso FastAPI

--- seção03/main.py
+++ seção03/main.py
@@ -38,6 +38,15 @@ async def post_curso(curso : Curso):
     del curso.id
     return curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id : int, curso : Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com ID {curso_id}.")
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
