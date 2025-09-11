638452560517900365527516274291e86eb4451a
EnzoBustos <enzobustos@usp.br>
2025-09-10 21:59:25 -0300

    Seção 03 - Curso FastAPI

---
+++ seção03/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+import typing
+
+app = FastAPI()
+
+cursos = {
+    1 : {
+        "titulo" : "Programação para Leigos",
+        "aulas" : 112,
+        "horas" : 58,
+    },
+    2 : {
+        "titulo" : "Algoritmos e Lógica de Programação",
+        "aulas" : 87,
+        "horas" : 67,
+    },
+}
+
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id : int):
+    curso = cursos[curso_id]
+    curso.update({"id" : curso_id})
+    return curso
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
